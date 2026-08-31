35b8a5b8e0df2d1471d75a38c4aa536edb1b0be6
Arshaan Sindhwani <asindhwani08@gmail.com>
2026-08-31 18:11:05 +0100

    Fix terraform state drift and stabilise storage account

--- storage.tf
+++ storage.tf
@@ -1,16 +1,8 @@
-resource "random_string" "storage_suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
 resource "azurerm_storage_account" "main" {
-  # Storage account names must be 3 to 24 characters, lowercase
-  # letters and numbers only, no hyphens. substr() caps the length so
-  # this stays valid regardless of how long project_name is.
-  name                     = substr("st${replace(var.project_name, "-", "")}${random_string.storage_suffix.result}", 0, 24)
+  name                     = "starshaanazurelabyyj88d"
   resource_group_name      = data.azurerm_resource_group.main.name
   location                 = data.azurerm_resource_group.main.location
+
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
