ccac6d869749742d1efbd3c962a894c9497cff94
Arshaan Sindhwani <asindhwani08@gmail.com>
2026-08-31 15:08:35 +0100

    Fix terraform formatting

--- compute.tf
+++ compute.tf
@@ -35,7 +35,7 @@ resource "azurerm_container_app" "main" {
 
   ingress {
     external_enabled = true
-    target_port       = 80
+    target_port      = 80
 
     traffic_weight {
       percentage      = 100
